6ec991ce33ae24362555b77e46ede845eb159074
Nathan Zook <nzook@whitepages.com>
2013-09-18 18:32:23 -0700

    Clean up remove_existing_lwrp.  Straw man for https://github.com/acrmp/chefspec/issues/215

--- lib/chefspec/monkey_patches/lwrp_base.rb
+++ lib/chefspec/monkey_patches/lwrp_base.rb
@@ -1,45 +1,44 @@
 if Chef::VERSION >= '11.0.0'
 
-  # Override Chef LWRP creation to remove existing class to avoid redefinition warnings.
-  class Chef
-    class Provider
-      # Chef provider for a resource
-      class LWRPBase < Provider
-        class << self
-          alias_method :old_build_from_file, :build_from_file
+  module ClearResourceOrProvider
+    def self.extended(klass)
+      place = Object
+      klass.name.split('::')[0...-1].each{|cname| place = place.const_get(cname)}
+      klass.send(:define_singleton_method, :spaces){ [klass, place] }
+      klass.send(:define_singleton_method, :build_from_file_without_cleaning, klass.method(:build_from_file))
+      class << klass ; self ; end.send :remove_method, :build_from_file
+      klass.send(:define_singleton_method, :build_from_file,
+                 klass.method(:build_from_file_with_cleaning))
+    end
 
-          # Override Opscode provider to remove any existing LWRP
-          #
-          # @param [String] cookbook_name The name of the cookbook
-          # @param [String] filename File to load as a LWRP
-          # @param [Chef::RunContext] run_context Context of a Chef Run
-          # @return [Chef::Provider] the created provider
-          def build_from_file(*args)
-            cookbook_name, filename = args[0,2]
-            remove_existing_lwrp(convert_to_class_name(filename_to_qualified_string(cookbook_name, filename)))
-            old_build_from_file(*args)
-          end
+    def self.extended(klass)
+      place = Object
+      klass.name.split('::')[0...-1].each{|cname| place = place.const_get(cname)}
+      klass.send(:define_singleton_method, :spaces){ [klass, place] }
+      class << klass
+        alias_method :build_from_file_without_cleaning, :build_from_file
+        alias_method :build_from_file, :build_from_file_with_cleaning
+      end
+    end
 
-          # Remove any existing Chef provider or resource with the specified name.
-          #
-          # @param [String] class_name The class name. Must be a valid constant name.
-          def remove_existing_lwrp(class_name)
-            [Chef::Resource::LWRPBase, Chef::Provider::LWRPBase, Chef::Resource, Chef::Provider].each do |resource_holder|
-              if RUBY_VERSION < '1.9'
-                if resource_holder.const_defined? class_name
-                  resource_holder.send(:remove_const, class_name)
-                end
-              else
-                if resource_holder.const_defined? class_name, false
-                  resource_holder.send(:remove_const, class_name)
-                end
-              end
-            end
-          end
-
+    def remove_existing_resource_or_provider(class_name)
+      spaces.each do |space|
+        opts = RUBY_VERSION < '1.9'  ? [] : [false]
+        if space.const_defined?(class_name, *opts)
+          old_class = space.send(:remove_const, class_name)
+          space.resource_classes.delete(old_class) if space.respond_to(:resource_classes)
         end
       end
     end
 
+    def build_from_file_with_cleaning(*args)
+      cookbook_name, filename = args[0,2]
+      remove_existing_resource_or_provider(convert_to_class_name(filename_to_qualified_string(cookbook_name, filename)))
+      build_from_file_without_cleaning(*args)
+    end
   end
+
+  Chef::Provider::LWRPBase.extend ClearResourceOrProvider
+  Chef::Resource::LWRPBase.extend ClearResourceOrProvider
+
 end
